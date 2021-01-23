31b98ec7f216d49b280b2d506c9e04f45690d3f4
petekozz <66498808+petekozz@users.noreply.github.com>
2021-01-23 14:23:24 -0800

    Update App.react.js

--- src/components/App.react.js
+++ src/components/App.react.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch} from 'react-redux';
+import CategoriesList from './CategoriesList';
+import {GET_PRODUCTS} from '../reducers/productsReducer';
+
 
 function App() {
   return <>HELLO WORLD</>;
+  
+  useEffect( () => {
+    dispatch(GET_PRODUCTS());
 }
 
 export default App;
